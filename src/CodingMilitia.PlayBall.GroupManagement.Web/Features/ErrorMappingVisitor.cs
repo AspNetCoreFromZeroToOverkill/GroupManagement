625f32925083f137014db03f19248f469e2caf7f
João Antunes <joao@antunes.dev>
2020-02-23 11:00:55 +0000

    Either type adjustments
    - Fixing Fold
    - Making use of Fold to simplify ActionResult -> Result

--- src/CodingMilitia.PlayBall.GroupManagement.Web/Features/ErrorMappingVisitor.cs
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/Features/ErrorMappingVisitor.cs
@@ -3,13 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CodingMilitia.PlayBall.GroupManagement.Web.Features
 {
-    public struct ResultMappingVisitor<TModel> : Error.IErrorVisitor<ActionResult<TModel>>
+    public readonly struct ErrorMappingVisitor<TModel> : Error.IErrorVisitor<ActionResult<TModel>>
     {
         public ActionResult<TModel> Visit(Error.NotFound result)
             => new NotFoundObjectResult(result.Message);
 
+
         public ActionResult<TModel> Visit(Error.Invalid result)
             => new BadRequestObjectResult(result.Message);
+
 
         public ActionResult<TModel> Visit(Error.Unauthorized result)
             => new UnauthorizedObjectResult(result.Message);
--- src/CodingMilitia.PlayBall.GroupManagement.Web/Features/ResultExtensions.cs
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/Features/ResultExtensions.cs
@@ -8,32 +8,36 @@ namespace CodingMilitia.PlayBall.GroupManagement.Web.Features
     public static class ResultExtensions
     {
         public static ActionResult<TValue> ToActionResult<TValue>(this Either<Error, TValue> result)
-            => result switch
-            {
-                Either<Error, TValue>.Left error
-                => error.Value.Accept<ResultMappingVisitor<TValue>, ActionResult<TValue>>(
-                    new ResultMappingVisitor<TValue>()),
+            => result.Fold(
+                left: error => ToErrorResult<TValue>(error),
+                right: success => ToSuccessResult(success, value => value));
 
-                Either<Error, TValue>.Right success => Map(success.Value, value => value)
-            };
-        
         public static ActionResult<TModel> ToActionResult<TValue, TModel>(
             this Either<Error, TValue> result,
             Func<TValue, TModel> valueMapper)
-            => result switch
-            {
-                Either<Error, TValue>.Left error
-                => error.Value.Accept<ResultMappingVisitor<TModel>, ActionResult<TModel>>(
-                    new ResultMappingVisitor<TModel>()),
+            => result.Fold(
+                left: error => ToErrorResult<TModel>(error),
+                right: success => ToSuccessResult(success, valueMapper)
+            );
 
-                Either<Error, TValue>.Right success => Map(success.Value, valueMapper)
-            };
+        public static ActionResult ToUntypedActionResult<TValue>(
+            this Either<Error, TValue> result,
+            Func<TValue, ActionResult> successMapper)
+            => result.Fold(
+                left: error => ToErrorResult(error),
+                right: successMapper);
 
-        private static ActionResult<TModel> Map<TValue, TModel>(
+        private static ActionResult<TModel> ToSuccessResult<TValue, TModel>(
             TValue result,
             Func<TValue, TModel> valueMapper)
             => result is Unit
                 ? (ActionResult<TModel>) new ContentResult()
                 : valueMapper(result);
+
+        private static ActionResult<TModel> ToErrorResult<TModel>(Error error)
+            => error.Accept<ErrorMappingVisitor<TModel>, ActionResult<TModel>>(new ErrorMappingVisitor<TModel>());
+        
+        private static ActionResult ToErrorResult(Error error)
+            => error.Accept<ErrorMappingVisitor<object>, ActionResult<object>>(new ErrorMappingVisitor<object>()).Result;
     }
 }
